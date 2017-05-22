71fe19f5eea74ba7dc7775af154b5fbd0f400d93
MiguelSilva96 <a74601@alunos.uminho.pt>
2017-05-22 04:12:55 +0100

    Bad Request 400

--- MonitorUDP.java
+++ MonitorUDP.java
@@ -43,7 +43,6 @@ class NotifyThread extends Thread {
         running = false;
         socket.close();
     }
-
 }
 
 
--- ReverseProxy.java
+++ ReverseProxy.java
@@ -2,38 +2,54 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class InfoServer {
+
+class InfoServer implements Comparable {
     protected double rtt;
     protected double lossRate;
     protected int nTcpCon;
     protected int rec, env, warn;
     protected int dup, lastAck;
+    protected double score;
     protected Calendar lastProbing;
 
     public InfoServer() {
         rtt = 0.0;
         lossRate = 0.0;
         nTcpCon  = 0;
-        rec = env = 0;
-        warn = -1;
+        rec   = env = 0;
+        warn  = -1;
+        score = 0;
     }
 
-    public void updateLoss() {
+    public void update() {
         double aux = (double)rec/env;
         if(aux <= 1)
             lossRate = 1 - aux;
         else
             lossRate = aux - 1;
+        score  = rtt + lossRate;
+        score += nTcpCon;
+    }
+
+    public int compareTo(Object o) {
+        if(!(o instanceof InfoServer))
+            return -1;
+        InfoServer s = (InfoServer)o;
+        if(score > s.score)
+            return 1;
+        else if(score < s.score)
+            return -1;
+        return 0;
     }
 
 }
 
 class CheckTimeOut extends Thread {
 
-    HashMap<InetAddress,InfoServer> table;
+    Map<InetAddress,InfoServer> table;
     boolean running;
 
-    public CheckTimeOut(HashMap<InetAddress,InfoServer> table) {
+    public CheckTimeOut(Map<InetAddress,InfoServer> table) {
         this.table = table;
         running = true;
     }
@@ -82,7 +98,7 @@ class CheckTimeOut extends Thread {
 }
 
 class Probing extends Thread {
-    HashMap<InetAddress,InfoServer> table;
+    Map<InetAddress,InfoServer> table;
     boolean running;
     DatagramSocket socket;
     String request;
@@ -93,7 +109,7 @@ class Probing extends Thread {
     byte[] receiveData;
     byte[] sendData;
 
-    public Probing(HashMap<InetAddress,InfoServer> table) {
+    public Probing(Map<InetAddress,InfoServer> table) {
         this.table = table;
         try {
             socket = new DatagramSocket(5555);
@@ -117,7 +133,7 @@ class Probing extends Thread {
                 String received = new String(receive.getData(), receive.getOffset(), 
                                              receive.getLength());
                 addr = receive.getAddress();
-                //System.out.println(received);
+                System.out.println(received);
 
                 if(received.contains("Available"))
                     sendProbing(received.split(" ")[1]);
@@ -144,7 +160,8 @@ class Probing extends Thread {
         else
             backEnd = table.get(addr);
 
-        if(numb == backEnd.warn) {
+        int d = numb - backEnd.warn;
+        if(d == 0) {
             backEnd.dup++;
             return;
         }
@@ -159,7 +176,7 @@ class Probing extends Thread {
                                   addr, 5555);
         backEnd.lastProbing = c;
         backEnd.env++;
-        backEnd.updateLoss();
+        backEnd.update();
         backEnd.warn = numb;
         socket.send(send);
     }
@@ -176,7 +193,7 @@ class Probing extends Thread {
             backEnd.dup++;
         else 
             backEnd.rec++;
-        backEnd.updateLoss();
+        backEnd.update();
         backEnd.lastAck = ac;
         printTable();
 
@@ -231,26 +248,58 @@ class Probing extends Thread {
 
 class TcpConnection extends Thread {
     Socket client;
-    HashMap<InetAddress, InfoServer> table;
+    Socket backEnd;
+    Map<InetAddress, InfoServer> table;
 
-    public TcpConnection(Socket client, HashMap<InetAddress, InfoServer> table) {
+    public TcpConnection(Socket client, Map<InetAddress, InfoServer> table) {
         this.client = client;
         this.table = table;
     }
 
     @Override
     public void run() {
-        for(Map.Entry<InetAddress, InfoServer> entry : table.entrySet()) {
-            InetAddress key  = entry.getKey();
-            InfoServer value = entry.getValue();
-            if(value.nTcpCon == 0) {
-                //Socket e siga
+        double score = -1;
+        try {
+            InetAddress ip = InetAddress.getByName("localhost");
+            for(Map.Entry<InetAddress, InfoServer> entry : table.entrySet()) {
+                InfoServer s = entry.getValue();
+                InetAddress i = entry.getKey();
+                if(s.score < score || score < 0) {
+                    score = s.score;
+                    ip = i;
+                }
             }
-        }
+            String str;
+            String hostStr = "Host: " + ip.toString().split("/")[1];
+            backEnd = new Socket(ip, ReverseProxy.PORT);
+            InputStreamReader be = new InputStreamReader(backEnd.getInputStream());
+            InputStreamReader cl = new InputStreamReader(client.getInputStream());
+            BufferedReader inBE  = new BufferedReader(be);
+            BufferedReader inCL  = new BufferedReader(cl);
+            PrintWriter outCL = new PrintWriter(client.getOutputStream(), true);
+            PrintWriter outBE = new PrintWriter(backEnd.getOutputStream(), true);            
+            str = inCL.readLine();
+            while(!str.equals("")) {
+                if(str.contains("Host:"))
+                    str = hostStr;
+                outBE.print(str+"\n\r");
+                outBE.flush();
+                str = inCL.readLine(); 
+            }
+            outBE.print("\n\r");
+            outBE.flush();
+            str = inBE.readLine();
+            while(str != null) {
+                outCL.println(str);
+                outCL.flush();
+                str = inBE.readLine();
+            }
+
+        } catch(Exception e) {}
 
     }
-
 }
+
 
 public class ReverseProxy {
     public static final int TIMEOUT = 20000;
@@ -261,7 +310,7 @@ public class ReverseProxy {
 
         ServerSocket socket = new ServerSocket(PORT);
         Socket client;
-        HashMap<InetAddress,InfoServer> table = new HashMap<>();
+        Map<InetAddress,InfoServer> table = new HashMap<>();
         Thread check = new CheckTimeOut(table);
         Thread probn = new Probing(table);
         check.start();
